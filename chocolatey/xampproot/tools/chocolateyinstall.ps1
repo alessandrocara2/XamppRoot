f13cd786f7d68033dee310bdcea74cd8efc95db8
Alessandro Carabelli <alessandro@carabelli.it>
2020-01-09 10:38:34 +0100

    Added chocolatey package files

---
+++ chocolatey/xampproot/tools/chocolateyinstall.ps1
@@ -0,0 +1,45 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/alessandrocara2/XamppRoot/releases/download/1.0/xampproot.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url
+  FileFullPath  = "$toolsDir\xampproot.exe"
+
+  softwareName  = 'xampproot*'
+
+  checksum      = '750BF63DE8970CF7DCDED9C69C3620E3957050C4DA065007B44208341F42099A'
+  checksumType  = 'sha256'
+  checksum64    = '750BF63DE8970CF7DCDED9C69C3620E3957050C4DA065007B44208341F42099A'
+  checksumType64= 'sha256'
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/VERYSILENT'
+}
+
+Get-ChocolateyWebFile @packageArgs
+#Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
