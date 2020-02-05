3ceab2badd347aef2913ad50900a138252826ac7
Alessandro Carabelli <alessandro@carabelli.it>
2020-02-05 12:19:18 +0100

    Completed readme and choco packages

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # XamppRoot
 XamppRoot is a command-line tool built to easily modify Xampp's localhost root folder.
 
+[![BuyMeACoffee](https://img.shields.io/badge/coffee-donate-yellow.svg?logo=buy-me-a-coffee)](https://buymeacoff.ee/alecarabelli)
+[![Chocolatey](https://img.shields.io/chocolatey/dt/xampproot?color=brightgreen&label=Chocolatey%20downloads)](https://chocolatey.org/packages/xampproot)
+
 ## Requirements
 Only working for Windows systems.
 
@@ -17,6 +20,15 @@ The default Xampp installing folder is assumed to be C:\\Xampp
 | -x     | Yes               | Modifies Xampp root folder              |
 
 ## Usage
+
+### Chocolatey package
+Installation and update with [Chocolatey package manager](https://chocolatey.org/packages/xampproot/)
+- Install package (`choco install xampproot`)
+- Close and re-open terminal
+- Type `xampproot` followed by the needed flags
+- Enjoy 😁
+
+### Local installation
 - Download and compile the project
 - Move the .exe in a suitable folder
 - Open terminal (administrative permits aren't required)
--- chocolatey/xampproot/tools/chocolateyinstall.ps1
+++ chocolatey/xampproot/tools/chocolateyinstall.ps1
@@ -4,42 +4,19 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://github.com/alessandrocara2/XamppRoot/releases/download/1.0/xampproot.exe'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
-  url64bit      = $url
-  FileFullPath  = "$toolsDir\xampproot.exe"
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  url64bit       = $url
+  FileFullPath   = "$toolsDir\xampproot.exe"
 
-  softwareName  = 'xampproot*'
+  softwareName   = 'xampproot*'
 
-  checksum      = '750BF63DE8970CF7DCDED9C69C3620E3957050C4DA065007B44208341F42099A'
-  checksumType  = 'sha256'
-  checksum64    = '750BF63DE8970CF7DCDED9C69C3620E3957050C4DA065007B44208341F42099A'
-  checksumType64= 'sha256'
+  checksum64     = '750BF63DE8970CF7DCDED9C69C3620E3957050C4DA065007B44208341F42099A'
+  checksumType64 = 'sha256'
 
-  validExitCodes= @(0, 3010, 1641)
-  silentArgs   = '/VERYSILENT'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/VERYSILENT'
 }
 
 Get-ChocolateyWebFile @packageArgs
-#Install-ChocolateyPackage @packageArgs
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
